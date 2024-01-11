b1b81d9548b1ba32b88854472af91c6ba5218215
RafaelFacchin <rafael.facchin@yahoo.com.br>
2024-01-11 17:52:51 -0300

    DES_3: LOGICA DE CORRIDA E GANHOS

--- Form1.cs
+++ Form1.cs
@@ -145,6 +145,170 @@ namespace Simulador_de_corrida_de_caes
 
         private void button2_Click(object sender, EventArgs e)
         {
+            int Winner;//QUEM GANHOU
+            button1.Enabled = false; //DESATIVA A INTERFACE DE APOSTA
+            button2.Enabled = false; //DESATIVA A INTERFACE DE APOSTA
+            radioButton1.Checked = false;
+            radioButton1.Enabled = false;
+            radioButton2.Checked = false;
+            radioButton2.Enabled = false;
+            radioButton3.Checked = false;
+            radioButton3.Enabled = false;
+
+            while (Cachorro1.Correr() == false &&
+                    Cachorro2.Correr() == false &&
+                    Cachorro3.Correr() == false &&
+                    Cachorro4.Correr() == false)
+            {
+                Application.DoEvents();//Permite que o Windows LIBERE o uso do mouse e teclado enquanto o programa executa
+                System.Threading.Thread.Sleep(1); //ATRASA um pouco o tempo de execucao
+
+            }
+            //VERIFICA SE UM DOS CACHORROS GANHOU
+            //CACHORRO 1
+            if (Cachorro1.Correr() == true)
+            {
+                Winner = 1;
+                this.CaraJoao.ColetarDinheiro(Winner);
+                button1.Enabled = true;
+                button2.Enabled = true;
+                if (aposta1 == true && ApJoao.Cachorro == Winner)
+                {
+                    CaraJoao.Carteira += 2 * ApJoao.Valor;
+                }
+                if (aposta1 == true && ApJoao.Cachorro == Winner)
+                {
+                    CaraJoao.Carteira += 2 * ApJoao.Valor;
+                }
+                if (aposta2 == true && ApBeto.Cachorro == Winner)
+                {
+                    CaraBeto.Carteira += 2 * ApBeto.Valor;
+                }
+                if (aposta3 == true && ApAlfredo.Cachorro == Winner)
+                {
+                    CaraAlfredo.Carteira += 2 * ApAlfredo.Valor;
+                }
+            }
+
+            //CACHORRO 2
+            if (Cachorro2.Correr() == true)
+            {
+                Winner = 2;
+                this.CaraJoao.ColetarDinheiro(Winner);
+                button1.Enabled = true;
+                button2.Enabled = true;
+                if (aposta1 == true && ApJoao.Cachorro == Winner)
+                {
+                    CaraJoao.Carteira += 2 * ApJoao.Valor;
+                }
+                if (aposta1 == true && ApJoao.Cachorro == Winner)
+                {
+                    CaraJoao.Carteira += 2 * ApJoao.Valor;
+                }
+                if (aposta2 == true && ApBeto.Cachorro == Winner)
+                {
+                    CaraBeto.Carteira += 2 * ApBeto.Valor;
+                }
+                if (aposta3 == true && ApAlfredo.Cachorro == Winner)
+                {
+                    CaraAlfredo.Carteira += 2 * ApAlfredo.Valor;
+                }
+            }
+
+            //CACHORRO 3
+            if (Cachorro3.Correr() == true)
+            {
+                Winner = 3;
+                this.CaraJoao.ColetarDinheiro(Winner);
+                button1.Enabled = true;
+                button2.Enabled = true;
+                if (aposta1 == true && ApJoao.Cachorro == Winner)
+                {
+                    CaraJoao.Carteira += 2 * ApJoao.Valor;
+                }
+                if (aposta1 == true && ApJoao.Cachorro == Winner)
+                {
+                    CaraJoao.Carteira += 2 * ApJoao.Valor;
+                }
+                if (aposta2 == true && ApBeto.Cachorro == Winner)
+                {
+                    CaraBeto.Carteira += 2 * ApBeto.Valor;
+                }
+                if (aposta3 == true && ApAlfredo.Cachorro == Winner)
+                {
+                    CaraAlfredo.Carteira += 2 * ApAlfredo.Valor;
+                }
+            }
+
+            //CACHORRO 4
+            if (Cachorro4.Correr() == true)
+            {
+                Winner = 4;
+                this.CaraJoao.ColetarDinheiro(Winner);
+                button1.Enabled = true;
+                button2.Enabled = true;
+                if (aposta1 == true && ApJoao.Cachorro == Winner)
+                {
+                    CaraJoao.Carteira += 2 * ApJoao.Valor;
+                }
+                if (aposta1 == true && ApJoao.Cachorro == Winner)
+                {
+                    CaraJoao.Carteira += 2 * ApJoao.Valor;
+                }
+                if (aposta2 == true && ApBeto.Cachorro == Winner)
+                {
+                    CaraBeto.Carteira += 2 * ApBeto.Valor;
+                }
+                if (aposta3 == true && ApAlfredo.Cachorro == Winner)
+                {
+                    CaraAlfredo.Carteira += 2 * ApAlfredo.Valor;
+                }
+            }
+
+            //ATUALIZAR LABELS DOS CARAS
+            CaraJoao.AtualizarLabels();
+            CaraBeto.AtualizarLabels();
+            CaraAlfredo.AtualizarLabels();
+
+            //RETORNO DOS CACHORROS NA POSICAO INICIAL!!!
+            Cachorro1.VoltarPosicaoInicial();
+            Cachorro2.VoltarPosicaoInicial();
+            Cachorro3.VoltarPosicaoInicial();
+            Cachorro4.VoltarPosicaoInicial();
+
+            //DESMARCAR OS "radioButton`s"
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            radioButton3.Checked = false;
+
+            //HABILITAR OS "radioButton`s"
+            radioButton1.Enabled = true; //Permite interagir com o radioButton1
+            radioButton2.Enabled = true; //Permite interagir com o radioButton2
+            radioButton3.Enabled = true; //Permite interagir com o radioButton3
+
+            //LIBERA as apostas p/ jogar novamente
+            aposta1 = false;
+            aposta2 = false;
+            aposta3 = false;
+
+            //ZERA as apostas PREVIAS
+            CaraJoao.LimparAposta();
+            CaraBeto.LimparAposta();
+            CaraAlfredo.LimparAposta();
+
+            //DESABILITA radioButton, caso o apostador esteja sem dinheiro
+            if (CaraJoao.Carteira == 0)
+            {
+                CaraJoao.MyRadioButton.Enabled = false;
+            }
+            if (CaraBeto.Carteira == 0) 
+            {
+                CaraBeto.MyRadioButton.Enabled = false;
+            }
+            if (CaraAlfredo.Carteira == 0)
+            {
+                CaraAlfredo.MyRadioButton.Enabled = false;
+            }
 
         }
     }
